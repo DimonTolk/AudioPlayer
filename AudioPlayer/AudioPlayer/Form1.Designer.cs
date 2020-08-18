286012251b2b132c64dd51baa9b1c1358544af3f
DimonTolk <dimontolk.1996@gmail.com>
2020-08-18 22:36:22 +0300

    Playlist, shuffle and loop tracks

--- AudioPlayer/AudioPlayer/Form1.Designer.cs
+++ AudioPlayer/AudioPlayer/Form1.Designer.cs
@@ -30,50 +30,54 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.playBtn = new System.Windows.Forms.Button();
+            this.pauseBtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.button3 = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.prevBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.shuffleBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // playBtn
             // 
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.Location = new System.Drawing.Point(162, 446);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 68);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.playBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("playBtn.BackgroundImage")));
+            this.playBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.playBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.playBtn.Location = new System.Drawing.Point(162, 446);
+            this.playBtn.Name = "playBtn";
+            this.playBtn.Size = new System.Drawing.Size(83, 68);
+            this.playBtn.TabIndex = 0;
+            this.playBtn.UseVisualStyleBackColor = true;
+            this.playBtn.Click += new System.EventHandler(this.playBtn_Click);
             // 
-            // button2
+            // pauseBtn
             // 
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Location = new System.Drawing.Point(396, 446);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(83, 68);
-            this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.pauseBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pauseBtn.BackgroundImage")));
+            this.pauseBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pauseBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pauseBtn.Location = new System.Drawing.Point(396, 446);
+            this.pauseBtn.Name = "pauseBtn";
+            this.pauseBtn.Size = new System.Drawing.Size(83, 68);
+            this.pauseBtn.TabIndex = 1;
+            this.pauseBtn.UseVisualStyleBackColor = true;
+            this.pauseBtn.Click += new System.EventHandler(this.pauseBtn_Click);
             // 
             // label1
             // 
@@ -111,51 +115,54 @@
             this.fileToolStripMenuItem.Text = "File";
             this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
             // 
-            // button3
+            // stopBtn
             // 
-            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.Location = new System.Drawing.Point(625, 446);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(83, 68);
-            this.button3.TabIndex = 5;
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.stopBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("stopBtn.BackgroundImage")));
+            this.stopBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.stopBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.stopBtn.Location = new System.Drawing.Point(625, 446);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(83, 68);
+            this.stopBtn.TabIndex = 5;
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.Filter = "MP3|*.mp3|WAVE|*.wav";
+            this.openFileDialog1.Multiselect = true;
             // 
-            // button4
+            // prevBtn
             // 
-            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
-            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button4.Location = new System.Drawing.Point(245, 148);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(83, 68);
-            this.button4.TabIndex = 6;
-            this.button4.UseVisualStyleBackColor = true;
+            this.prevBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("prevBtn.BackgroundImage")));
+            this.prevBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.prevBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.prevBtn.Location = new System.Drawing.Point(245, 115);
+            this.prevBtn.Name = "prevBtn";
+            this.prevBtn.Size = new System.Drawing.Size(83, 68);
+            this.prevBtn.TabIndex = 6;
+            this.prevBtn.UseVisualStyleBackColor = true;
+            this.prevBtn.Click += new System.EventHandler(this.prevBtn_Click);
             // 
-            // button5
+            // nextBtn
             // 
-            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
-            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button5.Location = new System.Drawing.Point(572, 148);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(83, 68);
-            this.button5.TabIndex = 7;
-            this.button5.UseVisualStyleBackColor = true;
+            this.nextBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nextBtn.BackgroundImage")));
+            this.nextBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.nextBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.nextBtn.Location = new System.Drawing.Point(572, 115);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(83, 68);
+            this.nextBtn.TabIndex = 7;
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(81, 71);
+            this.pictureBox1.Location = new System.Drawing.Point(96, 71);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(158, 145);
+            this.pictureBox1.Size = new System.Drawing.Size(143, 112);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
@@ -192,23 +199,68 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(212, 342);
             this.listBox1.TabIndex = 12;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedValueChanged);
+            // 
+            // shuffleBtn
+            // 
+            this.shuffleBtn.Location = new System.Drawing.Point(755, 157);
+            this.shuffleBtn.Name = "shuffleBtn";
+            this.shuffleBtn.Size = new System.Drawing.Size(49, 23);
+            this.shuffleBtn.TabIndex = 13;
+            this.shuffleBtn.Text = "Shuffle";
+            this.shuffleBtn.UseVisualStyleBackColor = true;
+            this.shuffleBtn.Click += new System.EventHandler(this.shuffleBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(810, 157);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(46, 23);
+            this.clearBtn.TabIndex = 14;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(880, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Loop";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(931, 166);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(979, 549);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.shuffleBtn);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.prevBtn);
+            this.Controls.Add(this.stopBtn);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pauseBtn);
+            this.Controls.Add(this.playBtn);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
@@ -227,21 +279,25 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button playBtn;
+        private System.Windows.Forms.Button pauseBtn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button stopBtn;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button prevBtn;
+        private System.Windows.Forms.Button nextBtn;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button shuffleBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- AudioPlayer/AudioPlayer/Form1.cs
+++ AudioPlayer/AudioPlayer/Form1.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -15,11 +14,15 @@ namespace AudioPlayer
 {
     public partial class Form1 : Form
     {
+        #region VARIABLES
         int angle = 5;
         bool isPaused = false;
         Image img;
-        string fileName = string.Empty;
         MediaPlayer player;
+        int curIndex = 0;
+        List<string> uploadedSongsUri = new List<string>();
+        bool isLooped = false;
+        #endregion
         public Form1()
         {
             InitializeComponent();
@@ -31,42 +34,95 @@ namespace AudioPlayer
             player = new MediaPlayer();
             trackBar1.Value = 50;
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            try
-            {
-                player.Open(new Uri(fileName));
-                player.Volume = trackBar1.Value/100.0;
-                player.Play();
-                timer1.Start();
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show($"{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            player.Close();
+        }
+        #region BUTTONS_EVENTS
+        private void playBtn_Click(object sender, EventArgs e)
+        {
+            Play();
+            listBox1.Enabled = false;
+            shuffleBtn.Enabled = false;
+            clearBtn.Enabled = false;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void pauseBtn_Click(object sender, EventArgs e)
         {
             if (isPaused)
             {
                 isPaused = false;
                 player.Play();
                 timer1.Start();
+                listBox1.Enabled = false;
+                shuffleBtn.Enabled = false;
+                clearBtn.Enabled = false;
             }
             else
             {
                 isPaused = true;
                 player.Pause();
                 timer1.Stop();
+                listBox1.Enabled = true;
+                shuffleBtn.Enabled = true;
+                clearBtn.Enabled = true;
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void stopBtn_Click(object sender, EventArgs e)
         {
             player.Stop();
             timer1.Stop();
+            listBox1.Enabled = true;
+            shuffleBtn.Enabled = true;
+            clearBtn.Enabled = true;
+        }
+        private void prevBtn_Click(object sender, EventArgs e)
+        {
+            curIndex--;
+            if (curIndex < 0 && isLooped)
+            {
+                curIndex = listBox1.Items.Count - 1; ;
+            }
+            Play();
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            uploadedSongsUri.Clear();
+        }
+
+        private void shuffleBtn_Click(object sender, EventArgs e)
+        {
+            ShuffleTracks();
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            curIndex++;
+            if(curIndex>=listBox1.Items.Count && isLooped)
+            {
+                curIndex = 0;
+            }
+            Play();
+        }
+        #endregion
+        private void Play()
+        {
+            try
+            {
+                listBox1.SelectedIndex = curIndex;
+                textBox1.Text = listBox1.SelectedItem.ToString();
+                player.Open(new Uri(uploadedSongsUri[curIndex]));
+                player.Volume = trackBar1.Value / 100.0;
+                player.Play();
+                timer1.Start();
+            }
+            catch
+            {
+                MessageBox.Show("Nothing to play", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void fileToolStripMenuItem_Click(object sender, EventArgs e)
@@ -74,20 +130,36 @@ namespace AudioPlayer
             ChooseMedia();
         }
 
-        public void ChooseMedia()
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            try
+            pictureBox1.Image = RotateImage(img, angle);
+            angle+=5;
+            if (angle > 360)
             {
-                if(openFileDialog1.ShowDialog() == DialogResult.OK)
-                {
-                    fileName = openFileDialog1.FileName;
-                    textBox1.Text = Path.GetFileName(fileName);
-                }
-
+                angle = 5;
             }
-            catch(Exception ex)
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            player.Volume = trackBar1.Value/100.0;
+        }
+
+        private void listBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = Path.GetFileName(listBox1.SelectedItem.ToString());
+            curIndex = listBox1.SelectedIndex;
+        }
+
+        private void ShuffleTracks()
+        {
+            var rnd = new Random();
+            uploadedSongsUri = uploadedSongsUri.OrderBy(o => rnd.Next()).ToList();
+
+            listBox1.Items.Clear();
+            foreach (var item in uploadedSongsUri)
             {
-                MessageBox.Show($"{ex.Message}", "Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                listBox1.Items.Add(Path.GetFileName(item));
             }
         }
 
@@ -111,25 +183,29 @@ namespace AudioPlayer
 
             return bmp;
         }
-
-        private void timer1_Tick(object sender, EventArgs e)
+        public void ChooseMedia()
         {
-            pictureBox1.Image = RotateImage(img, angle);
-            angle+=5;
-            if (angle > 360)
+            try
             {
-                angle = 5;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    foreach (var item in openFileDialog1.FileNames)
+                    {
+                        uploadedSongsUri.Add(item);
+                        listBox1.Items.Add(Path.GetFileName(item));
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            player.Close();
-        }
-
-        private void trackBar1_ValueChanged(object sender, EventArgs e)
-        {
-            player.Volume = trackBar1.Value/100.0;
+            isLooped = !isLooped;
         }
     }
 }
